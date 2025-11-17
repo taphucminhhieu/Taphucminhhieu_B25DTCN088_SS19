00c4e87984ef305331e525844da76a945410af75
taphucminhhieu <hta85055@gmail.com>
2025-11-17 23:01:46 +0700

    Delete bt4 ss18.cpp

--- bt4 ss18.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-	int a[5] ={1,2,3,4,5};
-	printf("gia tri bien a: %d\n",a);
-	for(int i=0;i<5;i++){
-		printf("dia chi cua phan tu a [%d]: %d \n",i, &a[i]); 
-	}
-	return 0;
-}
